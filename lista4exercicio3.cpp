4d68b348c0ab96c31c8ef3de52e57fe35ad624fd
Vitória Freitas <freitasvitoria712@gmail.com>
2023-04-30 17:42:27 -0300

    [add] exercicios de c++

--- lista4exercicio3.cpp
+++ lista4exercicio3.cpp
@@ -6,4 +6,58 @@
 //Subtração: z - w = (a – c) + (b – d)i
 //Multiplicação: z * w = (a*c – b*d) + (a*d + b*c)i
 //Divisão: z / w = (a*c + b*d)/(c2 + d2) + ((b*c – a*d)/ (c2 + d2))i
+#include <iostream>
+#include <vector> 
 
+using namespace std;
+
+float add_real(float a, float b) {
+    return a + b;
+}
+
+float add_imag(float a, float b) {
+    return a + b;
+}
+
+float sub_real(float a, float b) {
+    return a - b;
+}
+
+float sub_imag(float a, float b) {
+    return a - b;
+}
+
+float mul_real(float a, float b, float c, float d) {
+    return a * c - b * d;
+}
+
+float mul_imag(float a, float b, float c, float d) {
+    return a * d + b * c;
+}
+
+float div_real(float a, float b, float c, float d) {
+    float denominador = c * c + d * d;
+    return (a * c + b * d) / denominador;
+}
+
+float div_imag(float a, float b, float c, float d) {
+    float denominador = c * c + d * d;
+    return (b * c - a * d) / denominador;
+}
+
+int main() {
+    float z_real, z_imag, w_real, w_imag;
+    cout << "Informe o valor real de z: ";
+    cin >> z_real;
+    cout << "Informe o valor imaginario de z: ";
+    cin >> z_imag;
+    cout << "Informe o valor real de w: ";
+    cin >> w_real;
+    cout << "Informe o valor imaginario de w: ";
+    cin >> w_imag;
+    cout << "z + w = " << add_real(z_real, w_real) << " + " << add_imag(z_imag, w_imag) << "i" << endl;
+    cout << "z - w = " << sub_real(z_real, w_real) << " + " << sub_imag(z_imag, w_imag) << "i" << endl;
+    cout << "z * w = " << mul_real(z_real, z_imag, w_real, w_imag) << " + " << mul_imag(z_real, z_imag, w_real, w_imag) << "i" << endl;
+    cout << "z / w = " << div_real(z_real, z_imag, w_real, w_imag) << " + " << div_imag(z_real, z_imag, w_real, w_imag) << "i" << endl;
+    return 0;
+}
--- lista4exercicio4.cpp
+++ lista4exercicio4.cpp
@@ -1,1 +1,30 @@
 //4) Crie um programa que compara duas strings (não use a função strcmp
+
+#include <iostream>
+using namespace std;
+
+int comparaStrings(string str1, string str2) {
+    if (str1.length() != str2.length()) {
+        return 0;
+    }
+    for (int i = 0; i < str1.length(); i++) {
+        if (str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    string str1, str2;
+    cout << "Digite a primeira string: ";
+    cin >> str1;
+    cout << "Digite a segunda string: ";
+    cin >> str2;
+    if (comparaStrings(str1, str2)) {
+        cout << "As strings sao iguais" << endl;
+    } else {
+        cout << "As strings sao diferentes" << endl;
+    }
+    return 0;
+}
